f1a742920c2ee2cc2cf5a35742c8df7de900e240
User <User@DESKTOP-8BIP2SO>
2024-02-13 14:52:06 +0200

    Add project files.

---
+++ GitHubProject/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitHubProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            Console.WriteLine("Kazkas");
+        }
+    }
+}
