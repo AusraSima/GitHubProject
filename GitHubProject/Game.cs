b82172e7dfdf80bdb9d6ab31d7ae2d58c7c8855d
AusraS <ausrsima@yahoo.com>
2024-02-14 09:16:58 +0200

    Changed Program and created Game files

---
+++ GitHubProject/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubProject
+{
+    internal class Game
+    {
+
+        private string title;
+        private string type;
+        private int releaseYear;
+
+        public Game()
+        {
+        }
+
+        public Game(string title, string type, int releaseYear)
+        {
+            this.title = title;
+            this.type = type;
+            this.releaseYear = releaseYear;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public int ReleaseYear
+        {
+            get { return releaseYear; }
+            set { releaseYear = value; }
+        }
+    }
+}
--- GitHubProject/Program.cs
+++ GitHubProject/Program.cs
@@ -1,12 +1,57 @@
-﻿namespace GitHubProject
+﻿using static System.Reflection.Metadata.BlobBuilder;
+
+namespace GitHubProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine();
-            Console.WriteLine("Kazkas");
+            List<Game> games = new List<Game>();
+            games.Add(new Game("NOX", "action RPG", 2000));
+            games.Add(new Game("FoE", "strategy game", 2012));
+
+            while (true)
+            {
+                Console.WriteLine("Pasirinkite norima veiksma:");
+                Console.WriteLine("1 - perziureti zaidimus");
+                Console.WriteLine("2 - prideti zaidima");
+                Console.WriteLine("3 - redaguoti irasa");
+                Console.WriteLine("4 - istrinti zaidima");
+                Console.WriteLine("5 - iseiti");
+                Console.WriteLine("---------------------");
+                Console.WriteLine();
+
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        foreach (var g in games)
+                        {
+                            Console.WriteLine($"Game name: {g.Title}, game type: {g.Type}, release year: {g.ReleaseYear}");
+                        }
+                        break;
+                    case 2:
+                        Game game = new Game();
+                        Console.WriteLine("Iveskite zaidimo pavadinima");
+                        game.Title = Console.ReadLine();
+                        Console.WriteLine("Iveskite zaidimo tipa");
+                        game.Type = Console.ReadLine();
+                        Console.WriteLine("Iveskite zaidimo isleidimo metus");
+                        game.ReleaseYear = Convert.ToInt32(Console.ReadLine());
+                        games.Add(game);
+                        break;
+                    case 3:
+                        Console.WriteLine("3- redaguoti irasa");
+                        break;
+                    case 4:
+                        Console.WriteLine("4 - istrinti zaidima");
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+
+                }
+            }
         }
     }
 }
